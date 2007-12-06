275da91ae2917cb5986603a8da0d304b97a20a75
mag@localhost.localdomain <mag@localhost.localdomain>
2007-12-06 22:37:48 +0600

    added copyright blurb and short description

--- showtag.c
+++ showtag.c
@@ -1,3 +1,32 @@
+/*
+ * libid3v1 (http://libid3v1.dottedmag.net/)
+ *
+ * Example program for libid3v1 - reads the ID3v1 tags form the command-line
+ * arguments.
+ *
+ * Copyright (c) 2006,2007 Mikhail Gusarov <dottedmag@dottedmag.net>
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #include "libid3v1.h"
 #include <stdio.h>
 #include <unistd.h>
